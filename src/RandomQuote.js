c4fb91a3b16af521e0f2378ecfda7a968bc941b9
Sashka.Chi <cirtoska@gmail.com>
2022-08-19 07:26:01 +0200

    deploying

---
+++ src/RandomQuote.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import { FaQuoteLeft, FaTwitterSquare } from "react-icons/fa";
+
+function RandomQuote() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/quotes/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuote(data.quote);
+        setAuthor(data.author);
+      });
+  }, []);
+
+  let fetchNewQuote = () => {
+    fetch("https://dummyjson.com/quotes/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuote(data.quote);
+        setAuthor(data.author);
+      });
+  };
+  return (
+    <div id="quote-box">
+      <h3 id="text">
+        <span className="icon">
+          <FaQuoteLeft />
+        </span>
+        {quote}
+      </h3>
+      <p id="author">- {author} -</p>
+      <div className="buttons">
+        <button className="btn" id="new-quote" onClick={fetchNewQuote}>
+          New Quote
+        </button>
+        <a
+          href={
+            "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
+            encodeURIComponent('"' + quote + '" ' + author)
+          }
+          target="_blank"
+          id="tweet-quote"
+        >
+          <FaTwitterSquare />
+        </a>
+      </div>
+    </div>
+  );
+}
+export default RandomQuote;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      &copy;2022 Created by <em>Aleksandra Chirtoska</em>
+    </footer>
+  );
+};
+
+export default Footer;
